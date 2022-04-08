f508231e63db2f89db4c0107def0ce3e759ea092
AngerOverApathy <angersavestheworld@gmail.com>
2022-04-07 17:46:56 -0700

    refactor messaging

---
+++ controllers/messaging-routes.js
@@ -0,0 +1,26 @@
+
+const express = require('express')
+// const withAuth = require('../utils/auth')
+const router = express.Router()
+const path = require('path')
+
+router.get('/', (req,res) => {
+    res.sendFile(path.resolve('./public/messaging.html')) 
+})
+
+// router.get('*', (req, res) => {           //return index.html 
+//     res.sendFile(path.join('./public/home.html'));
+// })
+
+
+
+
+
+// const withAuth = require('../utils/auth')
+
+// router.get('/', (req,res) => {
+//     res.render('messaging')
+// })
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const http = require('http')
 
 const routes = require('./controllers')
 
@@ -10,8 +11,6 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const helpers = require("./utils/helpers");
 
 const sess = {
   secret: 'Super secret secret',
@@ -23,19 +22,58 @@ const sess = {
   })
 };
 
-app.use(session(sess));
+const server = http.createServer(app)
+const io = require('socket.io')(server) //for messaging
+const messaging = require('./controllers/messaging-route')
+const homepage = require('./controllers/dashboard-routes')
 
-const hbs = exphbs.create({ helpers });
+const sessionMiddleware = session(sess)
+app.use(sessionMiddleware);
+
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(__dirname + "/public"));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', messaging)
+// app.use('*', homepage)
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync({ force: true }).then(() => {
+  server.listen(PORT, () => console.log('Now listening'));
 });
+
+//for messaging
+const users = {}
+io.use((socket, next) => {
+  sessionMiddleware(socket.request, {}, next)
+})
+
+
+io.on('connection', socket => {   
+  const session = socket.request.session
+  session.connections++
+  session.save()  
+  socket.on('new-user', name => {
+    console.log(session)
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { 
+      message: message,
+      name: users[socket.id] })
+    })
+    socket.on('disconnecting', (name) => {
+      socket.broadcast.emit('user-disconnected', users[socket.id])
+      users[socket.id] = name
+      socket.broadcast.emit('user-connected', name)
+  })
+})
